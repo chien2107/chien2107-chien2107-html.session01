1ae62ccce340c181350d440371be67fddb8a5528
chien2107 <chien11112006@gmail.com>
2025-03-02 22:30:34 +0700

    session18

---
+++ ss18/baitap/bai4.js
@@ -0,0 +1,34 @@
+let num;
+num = +prompt("mời bạn nhập số (0 -> 9):");
+switch (num) {
+  case 1:
+    document.write("Một");
+    break;
+  case 2:
+    document.write("Hai");
+    break;
+  case 3:
+    document.write("Ba");
+    break;
+  case 4:
+    document.write("Bốn");  
+    break;
+  case 5:
+    document.write("Năm");
+    break;
+  case 6:
+    document.write("Sáu");
+    break;
+  case 7:
+    document.write("Bảy");
+    break;
+  case 8:
+    document.write("Tám");
+    break;
+  case 9:
+    document.write("Chín");
+    break;
+  default:
+    document.write("mời bạn nhập lại");
+    break;
+}
