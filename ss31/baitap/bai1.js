9aef93aa46558a9939e4b573364e592eb9afe0f5
chien2107 <chien11112006@gmail.com>
2025-03-20 10:09:48 +0700

    session31

---
+++ ss31/baitap/bai1.js
@@ -0,0 +1,6 @@
+function hideText() {
+    document.getElementById("h1").style.display = "none";
+}
+function showText() {
+    document.getElementById("h1").style.display = "block";
+}
---
+++ ss31/baitap/bai2.js
@@ -0,0 +1,5 @@
+function darkMode(){
+    h1E=document.getElementsByTagName("h1")[0].classList.toggle('backgroud');
+    pE=document.getElementsByTagName("p")[0].classList.toggle('backgroud');
+    bodyE=document.getElementsByTagName("body")[0].classList.toggle('backgroud');
+}
